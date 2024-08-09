4c3c8ba9f9449ff9ce0043f8b08e20fa042f9f31
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-09 21:33:41 +0200

    Script fixes speaker problems on Huawei Matebook D15: 'No sound'.

--- Scripts/Bash/speakers.sh
+++ Scripts/Bash/speakers.sh
@@ -6,6 +6,7 @@ Script configures Huawei MateBook D15 to work.
 Link:
 https://askubuntu.com/questions/1230363/internal-speaker-audio-is-not-working-in-ubuntu-20-04-lts-after-upgrading-from
 '
+
 # Activates the speakers as the sound card is not detected
 sudo add-apt-repository ppa:kaihengfeng/fix-lp1869819
 sudo apt upgrade
