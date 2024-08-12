f2c32d942631bf4d3d8987c2dfbd16fbf109d37d
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-12 14:58:06 +0200

    Script removes old revisions of snaps.

---
+++ Scripts/Bash/old_snaps.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+
+set -eu
+snap list --all | awk '/disabled/{print $1, $3}' |
+    while read -r snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
