4ec593572325d0578ad5136ec3746dd3d81ba5c3
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-28 14:06:54 +0200

    LazyVim: installation.

---
+++ Vim/LazyVim/install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+# Downloading and Installing Neovim
+wget "https://github.com/neovim/neovim/releases/download/v0.10.1/nvim-linux64.tar.gz"
+sudo mv nvim-linux64.tar.gz /usr/local/bin
+cd /usr/local/bin
+sudo tar xzvf nvim-linux64.tar.gz
+
+# Remove nvim-linux64.tar.gz, because we have the nvim-linux64 file
+rm nvim-linux64.tar.gz
+
+# Create a symbolic link
+sudo ln -s ./nvim-linux64/bin/nvim ./nvim
+
+# Back up current Neovim
+
+# (# required)
+mv ~/.config/nvim{,.bak}
+
+# (# optional but recommended)
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+# Remove the .git folder, so you can add it to your own repo later
+rm -rf ~/.config/nvim/.git
