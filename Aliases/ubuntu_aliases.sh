97d6cb1ec78b11b9833b62253b1167123c5ca63b
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-09-12 01:59:44 +0200

    Ubuntu: Bash aliases.

--- Aliases/ubuntu_aliases.sh
+++ Aliases/ubuntu_aliases.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# Navigation
+	# Navigation
 alias back="cd -"
 alias .="cd"
 alias ..="cd .."
@@ -8,9 +8,9 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
-# List files, directories and hidden files
+	# List files, directories and hidden files
 alias ls="colorls"
-alias hidden="ls -dA .*" # Show hidden files
+alias hidden="ls -dA .*"  # Show hidden files
 alias dir-contents="ls -d */"
 
 alias md="mkdir"
@@ -24,45 +24,45 @@ alias cppath="copypath"
 alias zshrc="code ~/.zshrc"
 alias bashrc="code ~/.bashrc"
 
-# Permissions        #  usr | grp | others
-alias x="chmod 755"     # rwx  | rwx | rwx
-alias x-="chmod 644"    # r = 4, w = 2, x = 1
-alias link="sudo ln -s" # sudo ln -s /path/to/file /path/to/symlink
+	# Permissions        #  usr | grp | others
+alias x="chmod 755"      # rwx  | rwx | rwx
+alias x-="chmod 644"     # r = 4, w = 2, x = 1
+alias link="sudo ln -s"  # sudo ln -s /path/to/file /path/to/symlink
 
-# Compress files and directories
-alias zip-dir="gzip -d" # Decompress files and directories
+	# Compress files and directories
+alias zip-dir="gzip -d"  # Decompress files and directories
 alias untar="sudo tar -zxvf"
 
 alias grep-e="grep -E"
 
-# List all users
+	# List all users
 alias list-users="cat /etc/passwd | grep -vE '(/bin/false|/sbin/nologin|/bin/sync)' | cut -d: -f1"
 alias ch-tafara="sudo chown tafara"
 alias ch-root="sudo chown root"
 
-# Add user
+	# Add user
 alias add-user="sudo adduser" # or sudo useradd new_username
 
-# Remove/delete a user, first you can use:
-alias del-user="sudo userdel" # username
+	# Remove/delete a user, first you can use:
+alias del-user="sudo userdel"  # username
 
-# Then you may want to delete the home directory for the deleted user account :
-alias rmv-home="sudo rm -r /home/" # username
+	# Then you may want to delete the home directory for the deleted user account :
+alias rmv-home="sudo rm -r /home/"  # username
 
-# Modify the username of a user
+	# Modify the username of a user
 alias ch-username="usermod -l new_username old_username"
 
-# Change the password for a user
-alias change-passwd="sudo passwd" # username
+	# Change the password for a user
+alias change-passwd="sudo passwd"   # username
 
-# To add a user to the sudo group
-alias add-sudo="usermod -aG sudo" # or adduser username sudo
+	# To add a user to the sudo group
+alias add-sudo="usermod -aG sudo"  # or adduser username sudo
 
-# Convert file to Unix or DOS format
+	# Convert file to Unix or DOS format
 alias dos-unix="dos2unix"
 alias unix-dos="unix2dos"
 
-# Restart the terminal
+	# Restart the terminal
 alias restart="source ~/.zshrc"
 alias refresh="exec \"$SHELL\""
 
@@ -87,27 +87,28 @@ alias requirements="pip3 freeze > requirements.txt"
 alias conda-requirements="pip list --format=freeze > requirements.txt"
 alias i-requirements="pip3 install -r requirements.txt"
 
-# Upgrading pip modules
+	# Upgrading pip modules
 alias pip-upgrade="pip3 install --upgrade pip"
 alias pip-uninstall="pip3 freeze | xargs pip uninstall -y"
 alias pip-up-old="pip3 list --outdated | cut -d ' ' -f 1 | xargs -n1 pip3 install -U"
 alias pip-up-all="pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U"
 
-# Django Project
-alias django-project="django-admin startproject" # name of the project
+	# Django Project
+alias django-project="django-admin startproject"  # name of the project
 alias django-server="python3 manage.py runserver"
-alias django-app="python3 manage.py startapp"              # name of the app
-alias django-migrations="python3 manage.py makemigrations" # creates a migration file
-alias django-migrate="python3 manage.py migrate"           # migrates data into a database
-alias django-shell="bpython manage.py shell"               # Django shell
+alias django-app="python3 manage.py startapp"  # name of the app
+alias django-migrations="python3 manage.py makemigrations"  # creates a migration file
+alias django-migrate="python3 manage.py migrate"  # migrates data into a database
+alias django-shell="bpython manage.py shell"  # Django shell
 alias django-superuser="python3 manage.py createsuperuser"
 
-# Flask Project
+	# Flask Project
 alias flask-run="flask run"
 alias flask-runapp="flask --app"
 alias flask-shell="flask shell"
 
-# Unit Testing
+
+	# Unit Testing
 alias test-file="python3 -m unittest"
 alias tests-dir="python3 -m unittest discover tests"
 
@@ -141,24 +142,27 @@ alias node-uninstall="nvm uninstall"
 
 # APT GET
 
-# Release version
+	# Release version
 alias release="lsb_release -a"
 
-# Packages list: Ubuntu
+	# Packages list: Ubuntu
 alias packages="sudo dpkg --get-selections > packages.txt"
 alias i-packages="sudo xargs -a packages.txt apt install"
 alias snap-list="snap list --all"
 
-# Packages list: VS Code
+	# Packages list: VS Code
 alias code-extensions="code --list-extensions > vscode-extensions.list"
 alias i-code-extensions="cat vscode-extensions.list | xargs -L 1 code --install-extension"
 
-# Search Packages
+	# Search Packages
 alias search="apt-cache search"
 alias show="apt-cache show"
 alias check="sudo apt-get check"
 
-# Install, update and upgrade
+	# Convert chm, epub to pdf
+alias to-pdf="ebook-convert"
+
+	# Install, update and upgrade
 alias reboot-now="sudo reboot now"
 alias poweroff="sudo shutdown now"
 alias i-dpkg="sudo dpkg -i"
@@ -170,32 +174,32 @@ alias full-upgrade="sudo apt-get full-upgrade"
 alias dist-upgrade="sudo apt-get dist-upgrade"
 alias update-atlas="sudo apt-get install --only-upgrade mongodb-atlas-cli"
 
-# Clean distro
+	# Clean distro
 alias set-zsh="p10k configure"
 alias autoclean="sudo apt-get autoclean"
 alias daemon-reload="sudo systemctl daemon-reload"
 
 alias list-running="sudo systemctl list-units --type=service --state=running"
 
-# Clear APT cache
+	# Clear APT cache
 alias show-cache="sudo du -sh /var/cache/apt"
 alias clean-cache="sudo apt-get clean"
 
-# Clear systemd journal
+	# Clear systemd journal
 alias journal-usage="journalctl --disk-usage"
 alias journal-clean="sudo journalctl --vacuum-time=2d"
 
-# Clear old snaps
-alias show-snaps="du -h /var/lib/snapd/snaps" # Then run rmv-old-snaps
+	# Clear old snaps
+alias show-snaps="du -h /var/lib/snapd/snaps"  # Then run rmv-old-snaps
 
-# Free up memory, OR: free-memory
+	# Free up memory, OR: free-memory
 alias free-mem="free -h && sudo sysctl -w vm.drop_caches=3 && sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches && free -h"
 
-# Clear Thumbnail Cache
+	# Clear Thumbnail Cache
 alias show-thumbnail="du -sh ~/.cache/thumbnails"
 alias clean-thumbnail="rm -rf ~/.cache/thumbnails/*"
 
-# Remove and purge
+	# Remove and purge
 alias purge="sudo apt-get purge"
 alias remove="sudo apt-get remove"
 alias autoremove="sudo apt autoremove"
@@ -207,15 +211,15 @@ alias k9="sudo kill -9"
 alias k6="sudo kill -6"
 alias k3="sudo kill -3"
 
-# Ports
+	# Ports
 alias ports="netstat -tuln"
-alias on-port="sudo lsof -i" # Show which process is listening on port, :port_number
+alias on-port="sudo lsof -i"      # Show which process is listening on port, :port_number
 
-# Using ss
-alias show-ports="sudo ss -ltn" # Show which ports are listening for connections
-alias proc-port="sudo ss -ltnp" # Show which process is listening on which port
+	# Using ss
+alias show-ports="sudo ss -ltn"  # Show which ports are listening for connections
+alias proc-port="sudo ss -ltnp"  # Show which process is listening on which port
 
-# Using nmap
+	# Using nmap
 alias nmap="sudo nmap localhost" # Scan localhost, we can change localhost to any IP address
 
 # UFW FIREWALL
@@ -241,16 +245,16 @@ alias ssh-enable="sudo systemctl enable ssh"
 
 # SSH SERVERS
 
-# Digital Ocean
+	# Digital Ocean
 alias d_ocean01="ssh digital-ocean"
 alias d_ocean02="ssh digital-ocean_01"
 
 # VIRTUAL MACHINES
 
-# Windows
+	# Windows
 alias win10="ssh win10"
 
-# Kali Linux
+	# Kali Linux
 alias kali="ssh kali-linux"
 
 # COPYING FILES AND DIRECTORIES
@@ -270,11 +274,11 @@ alias mysql-disable="sudo systemctl disable mysql"
 alias mysql="sudo mycli -u $MySQL -p $MySQL_PASSWD"
 alias mysql-tafara="sudo mycli -u $MySQL_USER -p $MySQL_PASSWD"
 
-# Connect and Execute MySQL Commands on the terminal
+	# Connect and Execute MySQL Commands on the terminal
 alias mysql-local="sudo mysql -hlocalhost -u $MySQL -p $MySQL_PASSWD"
 
-# Dump Database
-alias dump="mysqldump -u $MySQL -p $MySQL_PASSWD"
+	# Dump Database
+alias dump="mysqldump -u $MySQL -p"
 
 # Postgres
 alias pgsql-status="sudo systemctl status postgresql"
@@ -378,7 +382,7 @@ alias vagrant-enable="sudo systemctl enable vagrant"
 alias vagrant-disable="sudo systemctl disable vagrant"
 
 # vagrant Commands
-alias vagrant-remove="vagrant box remove" # Pass the box name
+alias vagrant-remove="vagrant box remove"  # Pass the box name
 
 # GIT
 alias add="git add"
@@ -388,69 +392,73 @@ alias init="git init"
 alias clone="git clone"
 alias status="git status"
 
-# Branches
+	# Branches
 alias branch="git branch"
 alias branch-merged="git branch --merged"
 alias checkout="git checkout"
 alias checkin="git checkout -b"
 
-# Pull conflicts
+	# Pull conflicts
 alias pull-norebase="git pull --no-rebase"
 alias push-origin="git push origin main"
 
-# Merge conflicts
+	# Merge conflicts
 alias merge="git merge"
 alias merge-abort="git merge --abort"
 alias merge-oneline="git log --merge --oneline"
 
-# Stash
+	# Stash
 alias stash="git stash"
 
-# Stash with untracked files
+	# Stash with untracked files
 alias stash-u="git stash -u"
 alias stash-untracked="git stash --include-untracked"
 
-# Stash list
+	# Stash list
 alias stash-list="git stash list"
 alias stash-clear="git stash clear"
 alias stash-pop="git stash pop"
 alias stash-apply="git stash apply"
 alias stash-push="git stash push -m"
 
-# Stash drop specific stash: n = index
+	# Stash drop specific stash: n = index
 alias stash-drop="git stash drop stash@{n}"
 
-# Obtain Stash Commit Hash
+	# Obtain Stash Commit Hash
 # alias stash-obtain="gitk --all $( git fsck --no-reflog | awk '/dangling commit/ {print $3}' )"
 
-# Show the tracked files in the stash: n = index
+	# Show the tracked files in the stash: n = index
 alias show-tracked="git show -p stash@{n}"
 
-# Show the untracked files in the stash
+	# Show the untracked files in the stash
 alias show-untracked="git show stash@{n}^3"
 
-# Commit
+	# Commit
 alias commit="git commit -m"
 alias add-commit="git commit -a -m"
 
-# Push
+	# Push
 alias push="git push"
 alias push-branch="git push -u origin"
 
-# Pull
+	# Pull
 alias pull="git pull"
+alias pull-main="git pull origin main"  # Pull from main branch
+alias pull-branch="git pull origin"  # Pass the branch name
+
+	# Fetch
 alias fetch="git fetch origin"
 
-# Delete
+	# Delete
 alias del-branch="git push origin --delete"
 
-# Delete committed and pushed: AFter deleting, commit and force-push
-alias rm-cache="git rm --cached" # Remove file from remote repo, but keeps it locally
+	# Delete committed and pushed: AFter deleting, commit and force-push
+alias rm-cache="git rm --cached"  # Remove file from remote repo, but keeps it locally
 alias delete-file="git rm"
 alias delete-folder="git rm -r"
 alias force-push="git push --force -u origin main"
 
-# Set username and email address
+	# Set username and email address
 alias name="git config --global user.name"
 alias email="git config --global user.email"
 alias git-list="git config --list"
@@ -461,15 +469,16 @@ alias lazy="lazygit"
 # If you change repos in lazygit and want your shell to change directory
 # into that repo on exiting lazygit, add this to your ~/.zshrc (or other rc file):
 
-lg() {
-	export LAZYGIT_NEW_DIR_FILE=~/.lazygit/newdir
+lg()
+{
+    export LAZYGIT_NEW_DIR_FILE=~/.lazygit/newdir
 
-	lazygit "$@"
+    lazygit "$@"
 
-	if [ -f $LAZYGIT_NEW_DIR_FILE ]; then
-		cd "$(cat $LAZYGIT_NEW_DIR_FILE)"
-		rm -f $LAZYGIT_NEW_DIR_FILE >/dev/null
-	fi
+    if [ -f $LAZYGIT_NEW_DIR_FILE ]; then
+            cd "$(cat $LAZYGIT_NEW_DIR_FILE)"
+            rm -f $LAZYGIT_NEW_DIR_FILE > /dev/null
+    fi
 }
 
 # Ansible
@@ -532,8 +541,8 @@ eval "$(pyenv virtualenv-init -)"
 
 # Node NVM
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Go
 export PATH=$PATH:/usr/local/go/bin
