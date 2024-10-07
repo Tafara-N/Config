c09f6f94c5b323ac088676bfcf34919b1b361f1f
Tafara <136159071+Tafara-N@users.noreply.github.com>
2024-10-07 22:07:25 +0200

    Create unicode.py

---
+++ unicode.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+What is Unicode:
+================
+Unicode is a standard that assigns a unique number for an abstract character.
+
+"Unicode provides a unique number for every character,
+no matter what the platform,
+no matter what the program,
+no matter what the language."
+                                    Unicode.org
+
+It
+
+
+* The numerical value assigned to a character is called a code point.
+
+"""
+
+chinese = "2009年，波音公司(Boeing)在查尔斯顿附近的新厂破土动工时，曾宣扬这里是最先进的制造中心\
+将制造一款世界上最先进的飞机。但在接下来的十年里，这家生产787梦想客机的工厂一直受到做\
+工粗糙和监管不力的困扰，危及航空安全。"
+
+print(chinese)
