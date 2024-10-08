83effd1b244b959b10dc3cf986eacca34e7f4fe0
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-10-08 16:40:39 +0200

    Script cleans '==<version>' in a requirements.txt file.

--- Scripts/Python/clean_requirements.py
+++ Scripts/Python/clean_requirements.py
@@ -9,30 +9,11 @@ import re
 import sys
 
 
-def clean_requirements_file(file_path):
-    # Open the file for reading
-    with open(file_path, 'r') as file:
-        contents = file.readlines()
+def main() -> None:
+    """
+    The main function to clean a requirements.txt file.
+    """
 
-    # Clean each line using regex
-    cleaned_contents = [re.sub(r"==.*", "", line) for line in contents]
-
-    # Join the cleaned lines back into a single string
-    cleaned_text = ''.join(cleaned_contents)
-
-    return cleaned_text
-
-
-def save_cleaned_file(file_path, cleaned_text):
-    # Define a new file name or use the same to overwrite
-    new_file_path = file_path.replace('.txt', '.txt')
-    # Open the new file for writing
-    with open(new_file_path, 'w') as file:
-        file.write(cleaned_text)
-    print(f"Cleaned file saved as: {new_file_path}")
-
-
-if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: ./script.py <path_to_requirements.txt>")
         sys.exit(1)
@@ -41,3 +22,49 @@ if __name__ == "__main__":
     cleaned_text = clean_requirements_file(file_path)
 
     save_cleaned_file(file_path, cleaned_text)
+
+
+def clean_requirements_file(file_path: str) -> str:
+    """
+    Clean the 'requirements.txt' file by removing the version numbers from each
+    line.
+
+    Arguments:
+        file_path: The path to the 'requirements.txt' file.
+
+    Returns:
+        str
+            The cleaned text from the 'requirements.txt' file.
+    """
+
+    with open(file_path, 'r') as file:
+        contents = file.readlines()
+
+    # Clean each line using regex
+    cleaned_contents = [re.sub(r"==.*", "", line) for line in contents]
+
+    cleaned_text = ''.join(cleaned_contents)
+
+    return cleaned_text
+
+
+def save_cleaned_file(file_path: str, cleaned_text: str) -> None:
+    """
+    Clean the 'requirements.txt' file and save the cleaned text to the same
+    file.
+
+    Arguments:
+        file_path: The 'requirements.txt' file to clean.
+        cleaned_text: The cleaned text from the 'requirements.txt' file.
+    """
+
+    # Overwrite the original file with the cleaned text
+    replaced_file = file_path.replace('.txt', '.txt')
+
+    with open(replaced_file, 'w') as file:
+        file.write(cleaned_text)
+    print(f"Cleaned file saved as: {replaced_file}")
+
+
+if __name__ == "__main__":
+    main()
