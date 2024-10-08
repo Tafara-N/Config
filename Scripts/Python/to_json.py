73bc84c752abfc9c566c59d7516e9857ff95c4fd
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-10-08 16:27:11 +0200

    Converting a CSV file to a JSON file.

--- Scripts/Python/to_json.py
+++ Scripts/Python/to_json.py
@@ -2,27 +2,47 @@
 
 """
 Converting a CSV file to a JSON file
+
+First check if the CSV file is encoded in UTF-8, if not convert it to UTF-8
+then continue with the conversion to JSON.
+
+AUTHOR: Tafara Nyamhunga
+GITHUB: Tafara-N
+
 """
 
+import chardet
 import csv
 import json
 
 from sys import argv
 
 
-def csv_to_json(csv_file, json_file):
+def main() -> None:
     """
-    Convert a CSV file to a JSON file
-
-    Parameters:
-        - csv_file: CSV file to convert
-        - json_file: JSON file to create
-
-    Returns:
-        - None
+    The main function to convert a CSV file to a JSON file.
     """
 
-    with open(csv_file, "r") as file:
+    csv_file: str = argv[1]
+    json_file: str = argv[2]
+    csv_to_json(csv_file, json_file)
+
+
+def csv_to_json(csv_file: str, json_file: str) -> None:
+    """
+    Convert a CSV file to a JSON file.
+
+    The CSV file to be converted is first checked and converted to UTF-8 if
+    it is not already encoded in UTF-8.
+
+    Arguments:
+        csv_file: The CSV file to convert to JSON.
+        json_file: The JSON file to create.
+    """
+
+    utf8_csv_file = to_utf8(filepath=csv_file)
+
+    with open(utf8_csv_file, "r") as file:
         reader = csv.DictReader(file)
         data = list(reader)
 
@@ -30,10 +50,46 @@ def csv_to_json(csv_file, json_file):
         json.dump(data, file, indent=2)
 
 
-def main():
-    csv_file = argv[1]
-    json_file = argv[2]
-    csv_to_json(csv_file, json_file)
+def to_utf8(*, filepath: str) -> str:
+    """
+    Check if a file is encoded in UTF-8 and convert it if it is not.
+
+    Takes a file path as input, reads the file, and checks if the file is
+    encoded in UTF-8.
+
+    Arguments:
+        filepath: The CSV file to check and convert to UTF-8.
+
+    Raises:
+        ValueError
+            If the encoding detection fails or no encoding is detected.
+
+    Returns:
+        str
+            The converted file path if the file is not encoded in UTF-8.
+    """
+
+    with open(filepath, "rb") as file:
+        raw_data = file.read()
+
+    try:
+        result = chardet.detect(raw_data)
+        encoding = result["encoding"]
+    except ValueError as error:
+        print(f"Error: {error}")
+        exit(1)
+
+    if encoding != "utf-8":
+        if encoding:
+            decoded_data = raw_data.decode(encoding)
+        else:
+            raise ValueError("Encoding detection failed.")
+        utf8_data = decoded_data.encode("utf-8")
+
+        with open(filepath, "wb") as utf8_file:
+            utf8_file.write(utf8_data)
+
+    return filepath
 
 
 if __name__ == "__main__":
